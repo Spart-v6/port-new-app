3e6d9db0f9ce3c6513eb22b39315ca57bfc41201
Spart-v6 <ankurnfssingh24@gmail.com>
2022-04-30 01:00:32 +0530

    added loading screen

--- src/App.js
+++ src/App.js
@@ -8,34 +8,48 @@ import Contact from "./Pages/Contact";
 import {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 import {AnimatePresence} from 'framer-motion';
+import { gsap } from "gsap-trial";
 
 function App() {
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLoading = () => { setIsLoading(false); }
 
+  // useEffect(()=>{
+  //  setTimeout(() => {
+  //   setIsLoading(true);
+  //   console.log("Loaded");
+  //   console.log("Running gsap")
+  //   gsap.to(".loadingNow",{duration:1, right:100,ease:"Expo.easeOut"},"-=1")
+  //   console.log("Runned gsap")
+  //  }, 5000);
+  // })
+
   useEffect(()=>{
-    window.addEventListener("load",handleLoading);
-    return () => window.removeEventListener("load",handleLoading);
+    gsap.to(".loadingNow",{duration:2, delay:2, right:"100%", ease:"Expo.easeInOut"})  
   },[])
 
   const location = useLocation();
 
   return (
     <>
-      <Sidebar>
-        
-        <AnimatePresence exitBeforeEnter initial={false}>
-          <Routes location={location} key={location.pathname}>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/project" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </AnimatePresence>
-      </Sidebar>
+    {/* Do smth like take 5 seconds to load and then do fade out effect and then show the content after 5 seconds only */}
+    {/* Also add some condition when scrolling down and up, (left and right black screen) so that when u do too much scroll up, it doesn't show the content which got hidden .. so do smth like if scroll is done == 1 (once) then only hide content.. do in Home! */}
+    <div className="loadingNow"></div>
+    
+    <Sidebar>  
+      <AnimatePresence exitBeforeEnter initial={false}>
+        <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+    </Sidebar>
 
+  
     </>
   );
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,32 +5,53 @@ import { motion } from "framer-motion";
 import RGBLetters from "../../Components/RGBLetters";
 import PageTransition from "../../Components/PageTransition";
 import { gsap } from "gsap-trial";
-import { BsArrowDown } from 'react-icons/bs';
-
+import { BsArrowDown } from "react-icons/bs";
 
 function Home() {
   const [letterClass, setLetterClass] = useState("text-animate");
-  const strArray = [ "w", "e", "b", " ", "d", "e", "v", "e", "l", "o", "p", "e", "r", ];
+  const strArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
 
   const onHover = () => {
-    setTimeout(() => {  setLetterClass("text-animate-hover"); }, 2000);
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 2000);
   };
 
-  useEffect(() => { return onHover();}, []);
+  useEffect(() => {
+    return onHover();
+  }, []);
 
   const [nameOfPage, setNameOfPage] = useState("Home");
-
 
   const [scrollDir, setScrollDir] = useState(0);
   // Scrolling up -> left -> 0
   // Scrolling dwn -> right -> 1
+  const [counterUp, setCounterUp] = useState(0);
+  const [counterDown, setCounterDown] = useState(0);
 
-  window.addEventListener('wheel', (event)=>{
-    if (event.deltaY < 0){
+  window.addEventListener("wheel", (event) => {
+    if (event.deltaY < 0) {
       setScrollDir(0); // scrolling up
-    }
-    else if (event.deltaY > 0){
+      setCounterUp(1);
+      setCounterDown(0);
+    } else if (event.deltaY > 0) {
       setScrollDir(1); // scrolling down
+      setCounterDown(1);
+      setCounterUp(0);
     }
   });
 
@@ -42,51 +63,107 @@ function Home() {
   }
   const [q, ref] = useSelector();
 
-  useEffect(() => {
-    gsap.to(q("#downArrow"), { scaleY: 0.2,
-      transformOrigin: 'center bottom',
-      borderBottomLeftRadius: '40%',
-      ease: "Expo.easeOut" , repeat:-1});
-  }, []);
+  // useEffect(() => {
+  //   gsap.to(q("#downArrow"), {
+  //     scaleY: 0.2,
+  //     transformOrigin: "center bottom",
+  //     borderBottomLeftRadius: "40%",
+  //     ease: "Expo.easeOut",
+  //     repeat: -1,
+  //   });
+  // }, []);
 
-
-  const slideColorRight = ()=>{
-    gsap.to(".intro .greeting .color", {width:"100vw", duration: 1.5, ease:"Expo.easeInOut"})
-    gsap.to(".intro .greeting .hello", {x:"55%", duration:1.4, ease:"Expo.easeInOut"})
-    gsap.to("#text-reveal",{clipPath:'polygon(0px 100%, 100% 100%, 100% 0%, 0% 0%)',opacity:1 ,y:0,delay:.5 , stagger:.3, duration:1.5, ease:"Expo.easeOut"})
-    gsap.to(".intro .text-zone .flat-button",{opacity:1, y:0, duration: .5, delay:1.4, ease:"Expo.easeOut"})
-  }
-  const slideColorLeft = ()=>{
-    gsap.to(".intro .greeting .color", {width:"50vw", duration: 1.5, ease:"Expo.easeInOut"})
-    gsap.to(".intro .greeting .hello", {x:0, duration:1.4, ease:"Expo.easeInOut"})
-    gsap.to("#text-reveal",{clipPath:'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)',opacity:0 ,y:50, stagger:.3, duration:1.5, ease:"Expo.easeOut"})
-    gsap.to(".intro .text-zone .flat-button",{opacity:0, y:50, duration: .5, ease:"Expo.easeOut"})
-  }
+  const slideColorRight = () => {
+    if (counterDown === 1 && counterUp === 0) {
+      gsap.to(".intro .greeting .color", {
+        width: "100vw",
+        duration: 1.5,
+        ease: "Expo.easeInOut",
+      });
+      gsap.to(".intro .greeting .hello", {
+        x: "55%",
+        duration: 1.4,
+        ease: "Expo.easeInOut",
+      });
+      gsap.to("#text-reveal", {
+        clipPath: "polygon(0px 100%, 100% 100%, 100% 0%, 0% 0%)",
+        opacity: 1,
+        y: 0,
+        delay: 0.5,
+        stagger: 0.3,
+        duration: 1.5,
+        ease: "Expo.easeOut",
+      });
+      gsap.to(".intro .text-zone .flat-button", {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        delay: 1.4,
+        ease: "Expo.easeOut",
+      });
+    }
+  };
+  const slideColorLeft = () => {
+    if (counterUp === 1 && counterDown === 0) {
+      gsap.to(".intro .greeting .color", {
+        width: "50vw",
+        duration: 1.5,
+        ease: "Expo.easeInOut",
+      });
+      gsap.to(".intro .greeting .hello", {
+        x: 0,
+        duration: 1.4,
+        ease: "Expo.easeInOut",
+      });
+      gsap.to("#text-reveal", {
+        clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+        opacity: 0,
+        y: 50,
+        stagger: 0.3,
+        duration: 1.5,
+        ease: "Expo.easeOut",
+      });
+      gsap.to(".intro .text-zone .flat-button", {
+        opacity: 0,
+        y: 50,
+        duration: 0.5,
+        ease: "Expo.easeOut",
+      });
+    }
+  };
 
   const tl = gsap.timeline();
-  
+
   // useEffect(()=>{
-  //   tl.to('#char_H',{"stroke-dashoffset": 0},);
-  //   console.log(document.getElementById("char_H").getTotalLength());
+  //   tl.to('#char_H',  {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.19');
+  //   tl.to('#char_E',  {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.18');
+  //   tl.to('#char_Y',  {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.17');
+  //   tl.to('#char_T',  {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.16');
+  //   tl.to('#char_H1', {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.15');
+  //   tl.to('#char_E1', {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.14');
+  //   tl.to('#char_R',  {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.13');
+  //   tl.to('#char_E2', {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.12');
+  //   tl.to('#char_',   {"stroke-dashoffset": 0, delay:0.1, duration:3, ease:"Expo.easeOut"},'-=0.11');
+
+  //   // console.log(document.getElementById("char_").getTotalLength());
+
   // },[])
-  
 
   return (
     <>
+      <PageTransition nameOfPage={nameOfPage} />
 
-      <PageTransition nameOfPage={nameOfPage}/>
-      
       <main className="wholeContainer">
-
         <div className="intro">
-          <section className= "greeting">
+          <section className="greeting">
             <div className="hello" id="hello">
-              <span>Hey There!</span>
 
-              {/* <div id="scroll-down" ref={ref}>
-                <BsArrowDown id="downArrow" />
-              </div> */}
-              
+            HEY THERE
+
+
+
+
+
             </div>
             <div className="color"></div>
             {scrollDir ? slideColorRight() : slideColorLeft()}
@@ -96,28 +173,32 @@ function Home() {
             <div className="text-zone">
               <h1>
                 <div id="text-reveal" className="dotHome">
-                  <RGBLetters letterClass={letterClass} strArray={[".","h","o","m","e","(",")",]} idx={15}/>
+                  <RGBLetters
+                    letterClass={letterClass}
+                    strArray={[".", "h", "o", "m", "e", "(", ")"]}
+                    idx={15}
+                  />
                 </div>
                 <div id="text-reveal">
                   <span style={{ color: "#986ee8", fontSize: "70px" }}>I</span>
                   <span style={{ fontSize: "69px" }}>'m &nbsp;</span>
-                    Ankur
-                    <br />
+                  Ankur
+                  <br />
                 </div>
                 <div id="text-reveal">
                   <span>Web Developer</span>
                 </div>
               </h1>
-              <h2 id="text-reveal">Front-end Developer/ Designer/ Competitive Coder/ Gamer</h2>
+              <h2 id="text-reveal">
+                Front-end Developer/ Designer/ Competitive Coder/ Gamer
+              </h2>
               <Link to="/contact" className="flat-button">
                 CONTACT ME
               </Link>
             </div>
           </section>
         </div>
-
       </main>
-
     </>
   );
 }
