d1653dd7d55403e53cd50ed4b9209f89e884112c
Spart-v6 <ankurnfssingh24@gmail.com>
2022-05-21 01:26:51 +0530

    contact form ready

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -4,6 +4,8 @@ import emailjs, { send } from "@emailjs/browser";
 import CustomCursor from '../../Components/CustomCursor';
 import "./style.scss";
 import {motion} from "framer-motion";
+import { gsap } from "gsap-trial";
+import $ from "jquery";
 
 function Contact() {
   const [nameOfPage, setNameOfPage] = useState("Contact");
@@ -16,13 +18,37 @@ function Contact() {
 
     emailjs.sendForm("service_r51pv6c","template_vlea9p9",refForm.current,"8AUwRY3fQK2dczNsj")
       .then((result) => {
-          console.log(result);
+          // console.log(result);
+          $("#name").val('');
+          $("#email").val('');
+          $("#msg").val('');
+          gsap.to("#circ1",{transform:"translate(-160px, -160px)", opacity: 0, duration: 1, ease: "Expo.easeInOut" });
+          gsap.to("#circ2",{transform:"translate(160px, 160px)", opacity: 0, duration: 1, ease: "Expo.easeInOut" });
+          gsap.to("#path2",{transform: "scale(1, 1) translate(0%, 0%)", duration: 1, ease: "Expo.easeInOut"});
+          gsap.to("#path2",{stroke:"#3dd022", duration: .7, delay: .7, ease: "Expo.easeInOut"});
+          gsap.to("#path2",{stroke:"#a32dff", duration: .5, delay: 1.3, ease: "Expo.easeInOut"});
+          
+          // resetting
+          gsap.to("#path2",{transform: "scale(1, -1) translate(0%, -3%)", delay: 2, duration: 1, ease: "Expo.easeInOut"});
+          gsap.to("#circ1",{duration:1, transform:"translate(0,0)", opacity:1, delay:2,  ease:"Expo.easeInOut"},);
+          gsap.to("#circ2",{duration:1, transform:"translate(0,0)", opacity:1, delay:2,  ease:"Expo.easeInOut"},);
         },
         (error) => {
           console.log(error);
         }
       );
   };
+
+  useEffect(() => {
+    gsap.to("#path1", {strokeDashoffset: 0, duration: 1, ease:"Circ.easeInOut"})
+    gsap.to("#path2", {strokeDashoffset: 0, duration: 1, ease:"Circ.easeInOut"})
+    gsap.to("#path2", {transform:"scale(1, -1) translate(0%, -3%)", delay:1, duration:1, ease: "Expo.easeInOut"});
+    gsap.to("#circ1",{duration:1, transform:"translate(0,0)", opacity:1, delay:1,  ease:"Expo.easeInOut"},);
+    gsap.to("#circ2",{duration:1, transform:"translate(0,0)", opacity:1, delay:1,  ease:"Expo.easeInOut"},);
+
+    gsap.to("#leftBigCirc",{top: 0, left: 0,opacity:1, duration:1, delay:1, ease:"Expo.easeInOut"})
+    gsap.to("#rightBigCirc",{bottom:0, right: 0,opacity:1, duration:1, delay:1, ease:"Expo.easeInOut"})
+  },[])
 
   return (
     <>
@@ -36,23 +62,23 @@ function Contact() {
 
             <div className="input-info">
               <h2>Let's talk. </h2>
-              <span>Enter your details.</span>
+              <span>Drop a message :)</span>
             </div>
 
             <div className="input-container">
 
               <div className="input-field-name">
-                <input type="text" id="name" className="form__input" required autoComplete="off" placeholder=" " />
+                <input type="text" id="name" name="name" className="form__input" required autoComplete="off" placeholder=" " />
                 <label htmlFor="name" className="form__label">Name</label>
               </div>
 
               <div className="input-field-email">
-                <input type="email" id="email" className="form__input" required autoComplete="off" placeholder=" "/>
+                <input type="email" id="email" name="email" className="form__input" required autoComplete="off" placeholder=" "/>
                 <label htmlFor="email" className="form__label">Email</label>
               </div>
             
               <div className="input-field-msg">
-                <textarea type="text" name="msg" className="form__input" id="msg" autoComplete="off"  placeholder=" "/>
+                <textarea type="text" name="message" className="form__input" id="msg" autoComplete="off"  placeholder=" "/>
                 <label htmlFor="msg" className="form__label">Message</label>
               </div>
 
@@ -61,15 +87,35 @@ function Contact() {
             </div>
 
             <motion.div whileTap={{scale: 0.8}} className="BtnParent">
-              <button class="up">Send</button>
+              <button className="up">Send</button>
             </motion.div>
           </form>
 
           <div className="leftDetails">
-            Message
+
+            <svg viewBox="0 0 383 264" fill="none" >
+                <path d="M377.5 259H5.5V5.5H377.5V259Z" stroke="#A32DFF" strokeWidth="10" strokeLinecap="round" id="path1"/>
+                <path d="M377 6L193 145.5L5.5 6" stroke="#A32DFF" strokeWidth="10" strokeLinecap="round" id="path2"/>
+                <path d="M224 254C224 234.433 227.854 215.058 235.342 196.98C242.83 178.903 253.805 162.477 267.641 148.641C281.477 134.805 297.903 123.83 315.98 116.342C334.058 108.854 353.433 105 373 105V254H224Z" fill="#F90077" id="circ2"/>
+                <path d="M6 155C25.567 155 44.9423 151.146 63.0198 143.658C81.097 136.17 97.523 125.195 111.359 111.359C125.195 97.523 136.17 81.097 143.658 63.02C151.146 44.942 155 25.567 155 6H6V155Z" fill="#A32DFF" id="circ1"/>
+            </svg>
+
           </div>
 
         </div>
+
+        <div id="leftBigCirc">
+          <svg width="350" height="350" viewBox="0 0 350 350" fill="none">
+            <path d="M0 350C45.9628 350 91.4752 340.947 133.939 323.358C176.402 305.768 214.987 279.988 247.488 247.488C279.988 214.987 305.768 176.402 323.358 133.94C340.947 91.4745 350 45.9628 350 0H0V350Z" fill="#A32DFF"/>
+          </svg>
+        </div>
+          
+        <div id="rightBigCirc">
+          <svg width="350" height="350" viewBox="0 0 350 350" fill="none">
+            <path d="M0 350C0 304.037 9.05305 258.526 26.6423 216.06C44.2316 173.598 70.0117 135.013 102.512 102.512C135.013 70.0117 173.598 44.2316 216.06 26.6423C258.526 9.05305 304.037 0 350 0V350H0Z" fill="#F90077"/>
+          </svg>
+        </div>
+
 
         
       </div>
