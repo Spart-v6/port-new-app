3a672e1008800995ec476852adf87bd048367ec8
Spart-v6 <ankurnfssingh24@gmail.com>
2022-05-27 13:17:48 +0530

    added spartan font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+// import './fonts/league-spartan/LeagueSpartan-Bold.otf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
